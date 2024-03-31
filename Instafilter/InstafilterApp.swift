730bf0d06170d6fbad9d6ea552f74f6622726515
shomil03 <shomil02020@gmail.com>
2024-03-31 10:21:00 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Shomil Singh on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
