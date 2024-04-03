8d0089b4d84a182c5e57c456fea3ad8f99e35cab
shomil03 <shomil02020@gmail.com>
2024-04-03 23:22:26 +0530

    learned how to change images using CIImage

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    
+    @State private var image : Image?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+    func loadImage(){
+//        image = Image(.gengar)
+//        load image in UIimage
+        let inputImage = UIImage(resource: .gengar)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentfiler = CIFilter.sepiaTone()
+        currentfiler.inputImage = beginImage
+//        currentfiler.intensity = 1
+        let amount = 1.0
+        let inputKeys = currentfiler.inputKeys
+        
+        if(inputKeys.contains(kCIInputIntensityKey)){
+            currentfiler.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if(inputKeys.contains(kCIInputRadiusKey)){
+            currentfiler.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if(inputKeys.contains(kCIInputScaleKey)){
+            currentfiler.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
+        
+        
+        
+        
+        
+//        ci -> cg -> ui -> swiftUI
+        guard let outputImage = currentfiler.outputImage else{return}
+        
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else{return}
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        image = Image(uiImage: uiImage)
     }
 }
 
